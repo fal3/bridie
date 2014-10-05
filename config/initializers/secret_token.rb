102773bb27b1b8e483e7256c9b393b9f5be6c4c3
Amir Alexander Fallah <fallaha@wit.edu>
2014-10-05 16:53:10 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bridie::Application.config.secret_key_base = '9e1b9a0569cf44cf45f06c8f1a03cc31da293ffd97435a599af0e8f7bc7719f532b80a2cf66bb0de2041eb270544d8331c96bc1b5a13b54237d0d0976d4d2c19'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bridie::Application.config.session_store :cookie_store, key: '_bridie_session'
